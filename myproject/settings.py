eb1e2dcc2d38cd0095a7320c722f0f0bf2f5d1e7
Sithika Methnula <methnulasithika535@gmail.com>
2026-08-03 01:23:48 +0530

    Add postgres database setup and dotenv config

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  
+
 """
 Django settings for myproject project.
 
@@ -74,8 +79,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
