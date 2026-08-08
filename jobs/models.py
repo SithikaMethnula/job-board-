e9f6049a00efc98be22e76e85f6b5dc674fc16df
Sithika Methnula <methnulasithika535@gmail.com>
2026-08-09 00:01:22 +0530

    feat: create job model and register it in django admin

---
+++ jobs/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+
+class Job(models.Model):
+   
+    employer = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name='jobs'
+    )
+    title = models.CharField(max_length=255) 
+    description = models.TextField() 
+    location = models.CharField(max_length=100) 
+    salary = models.CharField(max_length=100, blank=True, null=True) 
+    created_at = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return self.title
